b052581fab958a9e86da5fedd6109d15976f5556
dermayank <joshimayank456@gmail.com>
2019-02-23 20:42:39 +0530

    more features added

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import boto3
 import os
 from pathlib import Path
+import datetime
 
 class grey_cli:
 
@@ -11,7 +12,7 @@ class grey_cli:
 		     region_name="us-east-1"  #"ap-south-1"
 		)
 		self.ec2 = session.resource('ec2')
-		print("connected")
+		self.ec2_client = session.client('ec2')
 
 	def create_instances(self, instance_name="greypie", keyname="greypie_key"):
 
@@ -38,14 +39,22 @@ class grey_cli:
 
 
 	def display_instances(self):
-		filters = [{'Name': 'tag:Name', 'Values': ['greypie']}] 	#filter instance by name
 		Filters=[{'Name': 'instance-state-name', 'Values': ['running']}]  #filter instance by name
 		instances = self.ec2.instances.filter(Filters=filters)
 		
 		for instance in instances:
 			print(instance.id, instance.instance_type)
 
-	def instance_id_for_name(self, tag_name = ""):
+	def instance_id_for_name(self, tag_name = "greypie"):
+		filters = [{'Name': 'tag:Name', 'Values': [tag_name]}] 	#filter instance by name
+		instances = self.ec2.instances.filter(Filters=filters)
+		i = 1
+		instances_list = []
+		for instance in instances:
+			instances_list.append(instance.id)
+			print(str(i)+")",instance.id)
+			i+=1
+
 
 	def create_key(self,key_name = "greypie_key"):
 		cwd = os.getcwd()
@@ -58,6 +67,24 @@ class grey_cli:
 		else:
 			print("key already exit")
 
+	
+	def terminate_instance(self, instance_id):
+		self.ec2.instances.filter(InstanceIds=instance_id).terminate()
+
+
+	def create_image(self, id, img_name = "greypie-image"):
+		instance = self.ec2.Instance(id)
+		image = instance.create_image(
+		    Description='Image created on '+str(datetime.datetime.now()),
+		    Name= img_name,
+		    NoReboot=True
+			)
+
+		str_id = str(image)
+		print("Image created with name= ** "+img_name+" **and id= **"+str_id[14:-2]+" **")
+
+
+
 
 gcli = grey_cli()
-gcli.create_key()
+gcli.create_image("i-07fd3a5b48b8237a4")
